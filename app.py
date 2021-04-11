3a6b84a12666d9fdbdb4311cb6aa9ba4a3966270
Chrissantema93 <0927765@hr.nl>
2021-04-12 00:13:52 +0200

    two inputs dash

--- app.py
+++ app.py
@@ -8,16 +8,23 @@ import pandas as pd
 data = pd.read_csv(r'hypotheek.csv', sep=';')
 df = px.data.gapminder()
 all_continents = df.continent.unique()
-
-
+verstrekker = data['Verstrekker'].unique()
+rentevaste = data['Rentevaste periode'].unique()
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     dcc.Checklist(
         id="checklist",
         options=[{"label": x, "value": x} 
-                 for x in all_continents],
-        value=all_continents[3:],
+                 for x in verstrekker],
+        value=verstrekker,
+        labelStyle={'display': 'inline-block'}
+    ),
+    dcc.Checklist(
+        id="checklist2",
+        options=[{"label": x, "value": x} 
+                 for x in rentevaste],
+        value=rentevaste,
         labelStyle={'display': 'inline-block'}
     ),
     dcc.Graph(id="line-chart"),
@@ -25,11 +32,13 @@ app.layout = html.Div([
 
 @app.callback(
     Output("line-chart", "figure"), 
-    [Input("checklist", "value")])
-def update_line_chart(continents):
-    # mask = df.continent.isin(continents)
-    fig = px.line(data, 
-        x="Jaar", y="Netto salaris na hypotheeklasten", color='Rentepercentage', line_group="Rentevaste periode")
+    [Input("checklist", "value"),
+    Input("checklist2", "value")])
+def update_line_chart(verstrekkers, periodes):
+    mask = data['Verstrekker'].isin(verstrekkers)
+    mask2 = data['Rentevaste periode'].isin(periodes)
+    fig = px.line(data[mask & mask2], 
+        x="Jaar", y="Netto salaris na hypotheeklasten", color='Rentepercentage', line_group='Rentevaste periode')
     return fig
 
 if __name__ == '__main__':
